559daa632192240b90343ac22adfa08b6ae68901
nvdson2005 <ducsonds2005@gmail.com>
2025-10-17 00:15:07 +0700

    feat: finish home page and init login page

--- src/components/layout/PageFooter.vue
+++ src/components/layout/PageFooter.vue
@@ -1,5 +1,51 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="w-full h-full bg-gray-200">Footer Component</div>
+  <div
+    class="w-full bg-[var(--pure-light)] border-t-slate-200 border flex flex-col items-center py-10"
+  >
+    <div class="w-[80%] grid grid-cols-4 gap-4">
+      <div>
+        <div class="text-black mb-4 text-lg">Customer Service</div>
+        <div class="text-slate-600 text-sm leading-8 flex flex-col items-baseline">
+          <button class="basic-hyperlink">Help Center</button>
+          <button class="basic-hyperlink">How to Buy</button>
+          <button class="basic-hyperlink">Shipping & Delivery</button>
+          <button class="basic-hyperlink">Return & Refunds</button>
+          <button class="basic-hyperlink">Contact Us</button>
+        </div>
+      </div>
+      <div>
+        <div class="text-black mb-4 text-lg">About Us</div>
+        <div class="text-slate-600 text-sm leading-8 flex flex-col items-baseline">
+          <button class="basic-hyperlink">About Company</button>
+          <button class="basic-hyperlink">Careers</button>
+          <button class="basic-hyperlink">Press & News</button>
+          <button class="basic-hyperlink">Store Locations</button>
+          <button class="basic-hyperlink">Affiliate Program</button>
+        </div>
+      </div>
+      <div>
+        <div class="text-black mb-4 text-lg">Payment</div>
+        <div class="text-slate-600 text-sm leading-8 flex flex-col items-baseline">
+          <button class="basic-hyperlink">Payment Methods</button>
+          <button class="basic-hyperlink">Gift Cards</button>
+          <button class="basic-hyperlink">Promotions</button>
+          <button class="basic-hyperlink">Coupons</button>
+          <button class="basic-hyperlink">Rewards</button>
+        </div>
+      </div>
+      <div>
+        <div class="text-black mb-4 text-lg">Follow Us</div>
+        <div class="text-slate-600 text-sm leading-8 flex flex-col items-baseline">
+          <button class="basic-hyperlink">Facebook</button>
+          <button class="basic-hyperlink">Twitter</button>
+          <button class="basic-hyperlink">Instagram</button>
+          <button class="basic-hyperlink">LinkedIn</button>
+          <button class="basic-hyperlink">YouTube</button>
+        </div>
+      </div>
+    </div>
+    <div class="text-slate-500 text-sm mt-10">&copy; 2024 BuyIt. All rights reserved.</div>
+  </div>
 </template>
 <style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="relative w-full h-screen [var(--pure-light)] z-0">
+    <div class="absolute inset-0 -z-10 w-[65%] h-full">
+      <img
+        src="@/assets/images/hero-section.png"
+        alt="Background"
+        class="w-full h-full object-scale-down"
+      />
+    </div>
+    <div
+      class="w-[30%] flex flex-col items-center absolute rounded-2xl p-4 mx-4 top-1/4 right-0 bg-[var(--orange)]"
+    >
+      <h1 class="text-3xl font-bold mb-2 text-[var(--light-pink)]">Welcome To BuyIt</h1>
+      <h2 class="text-lg mb-6 text-[var(--pure-light)]">Sign in to continue</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        class="input-box w-full mb-4 border border-gray-300"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        class="input-box w-full mb-6 border border-gray-300"
+      />
+      <button
+        class="w-full bg-[var(--red)] text-white py-2 rounded hover:bg-blue-700 transition-colors duration-200"
+      >
+        Login
+      </button>
+      <p class="mt-4 text-sm text-[var(--pure-light)]">
+        Don't have an account?
+        <a href="#" class="text-blue-200 hover:underline">Sign Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped></style>
