06eadf9d5338ada830a5e430debd0202041fcceb
phngan05 <phngan100705@gmail.com>
2025-12-08 19:55:54 +0700

    update

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,6 @@
+import router from '@/router'
 import axios, { type AxiosInstance } from 'axios'
-
+import { notify } from '@/utils/notify'
 const apiClient: AxiosInstance = axios.create({
   // baseURL: `http://localhost:3001/api`,
   // baseURL: 'https://buy-it-backend-nine.vercel.app/api',
@@ -9,4 +10,17 @@ const apiClient: AxiosInstance = axios.create({
   withCredentials: true,
 })
 
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('username')
+      localStorage.removeItem('role')
+      localStorage.removeItem('id')
+      router.push('/login')
+      notify('Session expired. Please login again.', 'error')
+    }
+    return Promise.reject(error)
+  },
+)
 export default apiClient
--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,9 @@ import './assets/styles/tailwind.css'
 import './index.css'
 import '@/utils/enum'
 import 'vue3-toastify/dist/index.css'
+// import Vue3Toastify from 'vue3-toastify'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
